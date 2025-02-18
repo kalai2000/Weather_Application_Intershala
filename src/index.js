843bb8b27be6bdc0e48e50d8b96b18707d004094
kalai <kalaivendhan2000@gmail.com>
2025-02-18 22:06:43 +0530

    javascript ongoing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 const userlocation = document.getElementById("user-location"); 
 console.log(userlocation);
+
+
+WEATHER_API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=278c74b50867b67a7fb174370bb7a155&q=`;
+WEATHER_DATA_ENDPOINT = `https://api.openweathermap.org/data/2.5/onecall?appid=278c74b50867b67a7fb174370bb7a155&exclude=minutely&units=metric&`;
+
+function finduserLocation() {
+    fetch(WEATHER_DATA_ENDPOINT+"London")
+    .then((response)=>response.json())
+    .then((data)=>
+        {console.log(data);
+        });
+}
+
+finduserLocation();
